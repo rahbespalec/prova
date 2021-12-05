8902862b1bca1b2d3499a44c6f5a7f6c545dc6c5
Raíssa Bespalec Dalóia <79861204+rahbespalec@users.noreply.github.com>
2021-12-05 14:57:27 -0300

    Add files via upload

---
+++ js/functions.js
@@ -0,0 +1,7 @@
+$(function(){
+    $('.navbar ul.dropdown-menu li').click(function(){
+        let section = $(this).attr('section')
+        let scrollTop = $('section[section='+section+']').offset().top
+        $('html, body').animate({scrollTop:scrollTop}, 100)
+    })
+})
